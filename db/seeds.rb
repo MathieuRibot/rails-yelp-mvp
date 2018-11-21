9b6019508fafb87d4faf21ca2d010394f2e22f61
Mathieu Ribot <mribot@hotmail.fr>
2018-11-21 14:44:37 +0100

    challenge#2 try1 -no front- day 26

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating 5 restaurants .......'
+
+restaurants_attributes = [
+  {
+    name:         'Mario & Luigi',
+    address:      '24 rue Oberkampf 75011 Paris',
+    category:     'italian'
+  },
+  {
+    name:         'Mamma Roma',
+    address:      '24 rue St Maur 75011 Paris',
+    category:     'italian'
+  },
+  {
+    name:         'Shangai Delice',
+    address:      '12 avenue Parmentier 75011 Paris',
+    category:     'chinese'
+  },
+  {
+    name:         'Le Bistrot du coin de la rue',
+    address:      'place St Ambroise 75011 Paris',
+    category:     'french'
+  },
+  {
+    name:         'La frite en folie',
+    address:      '10 impasse du Plat Pays 75011 Paris',
+    category:     'belgian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+
+puts 'Finished .......!'
